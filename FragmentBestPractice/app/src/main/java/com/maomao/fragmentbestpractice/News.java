84e377e4d3677380d01da0c99eca5355cf4caca4
Xuan Tang <maomao@guest-wireless-upc-1607-10-120-119-205.usc.edu>
2017-09-23 17:36:22 -0700

    learn something about fragment

---
+++ FragmentBestPractice/app/src/main/java/com/maomao/fragmentbestpractice/News.java
@@ -0,0 +1,22 @@
+package com.maomao.fragmentbestpractice;
+
+/**
+ * Created by maomao on 9/23/17.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ FragmentTest/app/src/main/java/com/maomao/fragmenttest/LeftFragment.java
@@ -0,0 +1,19 @@
+package com.maomao.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by maomao on 9/23/17.
+ */
+
+public class LeftFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false); return view;
+    }
+}
